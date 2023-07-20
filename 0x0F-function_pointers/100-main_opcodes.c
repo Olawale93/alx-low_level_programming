679834de3902e6b002f15eee5c9c777ce2444d61
Olawale93 <olawaleadeoluwa@gmail.com>
2023-07-20 16:05:03 +0000

    functions_pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(1);
 	}
-	
+
 	bytes = atoi(argv[1]);
 
 	if (bytes < 0)
@@ -32,11 +32,11 @@ int main(int argc, char *argv[])
 	for (i = 0; i < bytes; i++)
 	{
 		if (i == bytes - 1)
-	{
-		printf("%02hhx\n", arr[i]);
-		break;
-	}
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
 		printf("%02hhx ", arr[i]);
 	}
-		return (0);
+	return (0);
 }
